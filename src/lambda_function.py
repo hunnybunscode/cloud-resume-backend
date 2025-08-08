b987f3826b79f74bfdb44913bdab8c16a6a7f8bf
HUNTER HAYS <hunter.hays.3.ctr@us.af.mil>
2025-08-08 11:11:53 -0400

    Initial backend (SAM) with API, Lambda, DynamoDB and CI/CD

---
+++ src/lambda_function.py
@@ -0,0 +1,18 @@
+﻿import boto3
+import os
+
+table = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+    table.update_item(
+        Key={'id': 'visitor_count'},
+        UpdateExpression='ADD #count :inc',
+        ExpressionAttributeNames={'#count': 'count'},
+        ExpressionAttributeValues={':inc': 1}
+    )
+    resp = table.get_item(Key={'id': 'visitor_count'})
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': str(resp['Item']['count'])
+    }
